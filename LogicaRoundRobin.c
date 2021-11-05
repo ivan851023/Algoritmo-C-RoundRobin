67176f63ff9299a80d92ee2b052f6dc3b9ee6b39
ivan851023 <ivhan35@gmail.com>
2021-11-05 13:01:03 -0500

    Update LogicaRoundRobin.c

--- LogicaRoundRobin.c
+++ LogicaRoundRobin.c
@@ -1,84 +1,63 @@
+#include <stdio.h>
 
-#include <iostream>
-
-/*at = Arrival time,
-bt = Burst time,
-time_quantum= Quantum time
-tat = Turn around time,
-wt = Waiting time*/
-
-using namespace std;
-
-int main(){
-	int i,n,time,remain,temps=0,time_quantum;
-
-	int wt=0,tat=0;
-
-	cout<<"Enter the total number of process="<<endl;
-	cin>>n;
-
-	remain=n;
-	// assigning the number of process to remain variable
-
-	int at[n];
-	int bt[n];
-	int rt[n];
-
-
-	cout<<"Enter the Arrival time, Burst time for All the processes"<<endl;
-	for(i=0;i<n;i++)
-	{
-	  cout<<"Arrival time for process "<<i+1<<endl;
-		cin>>at[i];
-		cout<<"Burst time for process "<<i+1<<endl;
-		cin>>bt[i];
-		rt[i]=bt[i];
-	}
-
-	cout<<"Enter the value of time QUANTUM:"<<endl;
-	cin>>time_quantum;
-
-	cout<<"\n\nProcess\t:Turnaround Time:Waiting Time\n\n";
-	for(time=0,i=0;remain!=0;)
-	{
-		if(rt[i]<=time_quantum && rt[i]>0)
-		{
-			time += rt[i];
-		
-			rt[i]=0;
-			temps=1;
-		}
-
-		else if(rt[i]>0)
-		{
-			rt[i] -= time_quantum;
-		
-			time += time_quantum;
-		
-		}
-
-		if(rt[i]==0 && temps==1)
-		{
-			remain--;
-			//Desplaying the result of wating, turn around time:
-			printf("Process{%d}\t:\t%d\t:\t%d\n",i+1,time-at[i],time-at[i]-bt[i]);
-			cout<<endl;
-
-			wt += time-at[i]-bt[i];
-			tat += time-at[i];
-			temps=0;
-		}
-
-		if(i == n-1)
-			i=0;
-		else if(at[i+1] <= time)
-			i++;
-		else
-			i=0;
-	}
-
-	cout<<"Average waiting time "<<wt*1.0/n<<endl;
-	cout<<"Average turn around time "<<tat*1.0/n<<endl;;
-
-	return 0;
+void main() {
+    int i, n, total = 0, x, counter = 0, qt;
+    int wt = 0, tt = 0, at[10], bt[10], temp[10];
+    double average_wait_time, average_turnaround_time;
+    printf("Enter the number of processes : ");
+    scanf("%d", &n);
+    x = n;
+    for(i = 0; i < n; i++)
+    {
+        printf("\nProcess %d...\n", i + 1);
+        printf("Enter Arrival Time: ");
+        scanf("%d", &at[i]);
+        printf("Enter Burst Time: ");
+        scanf("%d", &bt[i]);
+        temp[i] = bt[i];
+    }
+    printf("\nEnter Time Quantum: ");
+    scanf("%d", &qt);
+    printf("\n+-----+------------+--------------+-----------------+\n");
+    printf("| PID | BURST TIME | WAITING TIME | TURNAROUND TIME |\n");
+    printf("+-----+------------+--------------+-----------------+\n");
+    for(total = 0, i = 0; x != 0;)
+    {
+        if(temp[i] <= qt && temp[i] > 0)
+        {
+                total = total + temp[i];
+                temp[i] = 0;
+                counter = 1;
+        }
+        else if(temp[i] > 0)
+        {
+                temp[i] = temp[i] - qt;
+                total = total + qt;
+        }
+        if(temp[i] == 0 && counter == 1)
+        {
+                x--;
+                printf("| %2d  |     %2d     |      %2d      |       %2d        |\n", i + 1, bt[i], total - at[i] - bt[i], total - at[i]);
+                printf("+-----+------------+--------------+-----------------+\n");
+                wt = wt + total - at[i] - bt[i];
+                tt = tt + total - at[i];
+                counter = 0;
+        }
+        if(i == n - 1)
+        {
+                i = 0;
+        }
+        else if(at[i + 1] <= total)
+        {
+                i++;
+        }
+        else
+        {
+                i = 0;
+        }
+    }
+    average_wait_time = wt * 1.0 / n;
+    average_turnaround_time = tt * 1.0 / n;
+    printf("\nAverage Waiting Time: %lf", average_wait_time);
+    printf("\nAverage Turnaround Time: %lf\n", average_turnaround_time);
 }
