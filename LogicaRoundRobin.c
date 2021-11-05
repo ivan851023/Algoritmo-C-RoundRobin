306a010a9e4e1f41ef2e868f47820b463dc3d268
ivan851023 <ivhan35@gmail.com>
2021-11-05 12:00:12 -0500

    Update LogicaRoundRobin.c

--- LogicaRoundRobin.c
+++ LogicaRoundRobin.c
@@ -1,64 +1,84 @@
-#include<stdio.h>  
-#include<conio.h>  
-  
-void main()  
-{  
-     
-    int i, NOP, sum=0,count=0, y, quant, wt=0, tat=0, at[10], bt[10], temp[10];  
-    float avg_wt, avg_tat;  
-    printf(" Ingrese número de procesos en el CPU: ");  
-    scanf("%d", &NOP);  
-    y = NOP; 
-  
 
-for(i=0; i<NOP; i++)  
-{  
-printf("\n Ingrese el tiempo de rafaga y el de llegada[%d]\n", i+1);  
-printf(" Tiempo Llegada es : \t");  
-scanf("%d", &at[i]);  
-printf(" \nTiempo Ráfaga es : \t"); 
-scanf("%d", &bt[i]);  
-temp[i] = bt[i]; 
-}  
+#include <iostream>
 
-printf("Ingrese el quantum: \t");  
-scanf("%d", &quant);  
+/*at = Arrival time,
+bt = Burst time,
+time_quantum= Quantum time
+tat = Turn around time,
+wt = Waiting time*/
 
-printf("\n Proceso No \t\t Tiempo Ráfaga \t\t Tiempo Respuesta \t\t Tiempo Espera");  
-for(sum=0, i = 0; y!=0; )  
-{  
-if(temp[i] <= quant && temp[i] > 0) 
-{  
-    sum = sum + temp[i];  
-    temp[i] = 0;  
-    count=1;  
-    }     
-    else if(temp[i] > 0)  
-    {  
-        temp[i] = temp[i] - quant;  
-        sum = sum + quant;    
-    }  
-    if(temp[i]==0 && count==1)  
-    {  
-        y--;   
-        printf("\nProceso No[%d] \t\t %d\t\t\t\t %d\t\t\t %d", i+1, bt[i], sum-at[i], sum-at[i]-bt[i]);  
-        wt = wt+sum-at[i]-bt[i];  
-        tat = tat+sum-at[i];  
-        count =0;     
-    }  
-    if(i==NOP-1)  
-    {  
-        i=0;  
-    }  
-    else if(at[i+1]<=sum)  
-    {  
-        i++;  
-    }  
-    else  
-    {  
-        i=0;  
-    }  
-}  
+using namespace std;
 
- 
-}  
+int main(){
+	int i,n,time,remain,temps=0,time_quantum;
+
+	int wt=0,tat=0;
+
+	cout<<"Enter the total number of process="<<endl;
+	cin>>n;
+
+	remain=n;
+	// assigning the number of process to remain variable
+
+	int at[n];
+	int bt[n];
+	int rt[n];
+
+
+	cout<<"Enter the Arrival time, Burst time for All the processes"<<endl;
+	for(i=0;i<n;i++)
+	{
+	  cout<<"Arrival time for process "<<i+1<<endl;
+		cin>>at[i];
+		cout<<"Burst time for process "<<i+1<<endl;
+		cin>>bt[i];
+		rt[i]=bt[i];
+	}
+
+	cout<<"Enter the value of time QUANTUM:"<<endl;
+	cin>>time_quantum;
+
+	cout<<"\n\nProcess\t:Turnaround Time:Waiting Time\n\n";
+	for(time=0,i=0;remain!=0;)
+	{
+		if(rt[i]<=time_quantum && rt[i]>0)
+		{
+			time += rt[i];
+		
+			rt[i]=0;
+			temps=1;
+		}
+
+		else if(rt[i]>0)
+		{
+			rt[i] -= time_quantum;
+		
+			time += time_quantum;
+		
+		}
+
+		if(rt[i]==0 && temps==1)
+		{
+			remain--;
+			//Desplaying the result of wating, turn around time:
+			printf("Process{%d}\t:\t%d\t:\t%d\n",i+1,time-at[i],time-at[i]-bt[i]);
+			cout<<endl;
+
+			wt += time-at[i]-bt[i];
+			tat += time-at[i];
+			temps=0;
+		}
+
+		if(i == n-1)
+			i=0;
+		else if(at[i+1] <= time)
+			i++;
+		else
+			i=0;
+	}
+
+	cout<<"Average waiting time "<<wt*1.0/n<<endl;
+	cout<<"Average turn around time "<<tat*1.0/n<<endl;;
+
+	return 0;
+}
